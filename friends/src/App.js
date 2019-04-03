bacecc311bdb3e6d6423f73fde187fea0d0505d5
Duc Vu <de.yd315@gmail.com>
2019-04-03 18:57:39 -0500

    added axious dependency

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
+  componentDidMount() {
+    axios
+    .get('http://localhost:5000/friends')
+    .then(res => {
+      console.log(res);
+      this.setState({ friends: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="Friends List">
+          <Friends friends={this.state.friends}/>
         </header>
       </div>
     );
@@ -26,3 +33,19 @@ class App extends Component {
 }
 
 export default App;
+
+
+export const Friends = props => {
+  console.log(props);
+  return (
+    <ul>
+      {/* {props.friends.map(friend => (
+        <li>
+          <p>friend.name</p>
+          <p>friend.age</p>
+          <p>friend.email</p>
+        </li>
+      ))} */}
+    </ul>
+  )
+}
